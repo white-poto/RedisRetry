09e78931d4039f612732c05ad0b5d9c94f02a591
huyanping <hypxm@qq.com>
2015-05-09 07:45:35 +0000

    add simple test

--- tests/simple.php
+++ tests/simple.php
@@ -6,3 +6,13 @@
  * Time: 15:20
  */
 
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . 'autoload.php';
+
+class Redis extends \Jenner\RedisRetry\Redis{}
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+$set_result = $redis->set('1', 1);
+var_dump($set_result);
+$get_result = $redis->get('1');
+var_dump($get_result);
